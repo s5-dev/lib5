fad664e719288ff7a849ea6f295e37fa2045643c
redsolver <info@redsolver.net>
2023-01-22 18:59:34 +0100

    Add storage_service lib file

---
+++ lib/storage_service.dart
@@ -0,0 +1,6 @@
+library lib5;
+
+export 'src/storage_service/config.dart';
+export 'src/storage_service/login.dart';
+export 'src/storage_service/register.dart';
+export 'src/api/remote_upload.dart';
