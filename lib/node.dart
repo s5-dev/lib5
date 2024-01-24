a1dc2af6445344126ade4459b1f8cd59a403d95c
redsolver <info@redsolver.net>
2024-01-24 19:33:03 +0100

    Remove broken import

--- lib/node.dart
+++ lib/node.dart
@@ -2,7 +2,6 @@ library lib5;
 
 export 'src/node/node.dart';
 export 'src/node/store.dart';
-export 'src/node/model/node_id.dart';
 export 'src/node/model/signed_message.dart';
 export 'src/node/service/p2p.dart';
 export 'src/node/service/registry.dart';
