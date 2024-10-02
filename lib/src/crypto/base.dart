acf2ca7421e44f07b52c2a8c81c9a5606628c82d
redsolver <info@redsolver.net>
2024-10-03 00:14:32 +0200

    Update CryptoImplementation base to match v1 spec

--- lib/src/crypto/base.dart
+++ lib/src/crypto/base.dart
@@ -1,26 +1,32 @@
+///
+/// This implementation follows the S5 v1 spec at https://docs.sfive.net/spec/api-interface.html
+///
+
 import 'dart:typed_data';
 
 import 'package:lib5/src/constants.dart';
 import 'package:lib5/src/util/typedefs.dart';
 
 abstract class CryptoImplementation {
-  Uint8List generateRandomBytes(int length);
+  Uint8List generateSecureRandomBytes(int length);
 
   Future<Uint8List> hashBlake3(Uint8List input);
+
   Uint8List hashBlake3Sync(Uint8List input);
+
   Future<Uint8List> hashBlake3File({
     required int size,
     required OpenReadFunction openRead,
   });
 
   Future<bool> verifyEd25519({
-    required Uint8List pk,
+    required Uint8List publicKey,
     required Uint8List message,
     required Uint8List signature,
   });
 
   Future<Uint8List> signEd25519({
-    required KeyPairEd25519 kp,
+    required KeyPairEd25519 keyPair,
     required Uint8List message,
   });
 
