994ab52855ae1fd7894f2d2aa04824caa68ab32d
redsolver <info@redsolver.net>
2024-10-03 00:13:30 +0200

    Update S5APIProvider base to match v1 spec

--- lib/src/api/base.dart
+++ lib/src/api/base.dart
@@ -1,3 +1,7 @@
+///
+/// This implementation follows the S5 v1 spec at https://docs.sfive.net/spec/api-interface.html
+///
+
 import 'dart:typed_data';
 
 import 'package:lib5/src/crypto/base.dart';
@@ -10,6 +14,42 @@ import 'package:lib5/src/stream/message.dart';
 import 'package:lib5/src/util/typedefs.dart';
 
 abstract class S5APIProvider {
+  /// Blocks until the S5 API is initialized and ready to be used
+  Future<void> ensureInitialized();
+
+  /// Upload a small blob of bytes
+  ///
+  /// Returns the Raw CID of the uploaded raw file blob
+  ///
+  /// Max size is 10 MiB, use [uploadRawFile] for larger files
+  Future<BlobCID> uploadBlob(Uint8List data);
+
+  /// Upload a raw file
+  ///
+  /// Returns the Raw CID of the uploaded raw file blob
+  ///
+  /// Does not have a file size limit and can handle large files efficiently
+  Future<BlobCID> uploadBlobWithStream({
+    required Multihash hash,
+    required int size,
+    required OpenReadFunction openRead,
+  });
+
+  /// Downloads a full file blob to memory, you should only use this if they are smaller than 1 MB
+  Future<Uint8List> downloadBlob(Multihash hash, {Route? route});
+
+  /// Downloads a slice of a blob to memory, from `start` (inclusive) to `end` (exclusive)
+  Future<Uint8List> downloadBlobSlice(
+    Multihash hash, {
+    required int start,
+    required int end,
+    Route? route,
+  });
+
+  Future<void> pinHash(Multihash hash);
+
+  Future<void> unpinHash(Multihash hash);
+
   Future<SignedRegistryEntry?> registryGet(
     Uint8List pk, {
     Route? route,
@@ -33,32 +73,6 @@ abstract class S5APIProvider {
     SignedStreamMessage msg, {
     Route? route,
   });
-
-  /// Upload a small blob of bytes
-  ///
-  /// Returns the Raw CID of the uploaded raw file blob
-  ///
-  /// Max size is 10 MB, use [uploadRawFile] for larger files
-  Future<CID> uploadBlob(Uint8List data);
-
-  /// Upload a raw file
-  ///
-  /// Returns the Raw CID of the uploaded raw file blob
-  ///
-  /// Does not have a file size limit and can handle large files efficiently
-  Future<CID> uploadRawFile({
-    required Multihash hash,
-    required int size,
-    required OpenReadFunction openRead,
-  });
-
-  /// Downloads a full file blob to memory, you should only use this if they are smaller than 1 MB
-  Future<Uint8List> downloadRawFile(Multihash hash, {Route? route});
-
-  /// Download Metadata by its CID
-  Future<Metadata> downloadMetadata(CID cid, {Route? route});
-
-  Future<void> deleteCID(CID cid);
 
   CryptoImplementation get crypto;
 }
